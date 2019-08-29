7a52c3998e219ba55fd88aeed383ad7b01f1f269
yu874721995 <yubei874721995@163.com>
2019-08-29 11:20:02 +0800

    '再试试'

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+@Time    : 2019/8/29 11:19
+@Author  : Careslten
+@Site    : 
+@File    : __init__.py
+@Software: PyCharm
+'''
+
+import pymysql
+pymysql.install_as_MySQLdb()
