550808fcc74bcd7134f83c332ecf9b18c09d5d7e
yu874721995 <yubei874721995@163.com>
2019-10-08 18:47:30 +0800

    '1'

--- oneGo/api/wechat.py
+++ oneGo/api/wechat.py
@@ -37,7 +37,12 @@ def sendMsg(request):
                 if message.content == 'wechat':
                     response = wechat.response_text(u'^_^')
                 else:
-                    response = wechat.response_text(u'别问，问就是没调好')
+                    x = urllib.parse.quote(message.content)
+                    link = urllib.request.urlopen(
+                        "http://nlp.xiaoi.com/robot/webrobot?&callback=__webrobot_processMsg&data=%7B%22sessionId%22%3A%22ff725c236e5245a3ac825b2dd88a7501%22%2C%22robotId%22%3A%22webbot%22%2C%22userId%22%3A%227cd29df3450745fbbdcf1a462e6c58e6%22%2C%22body%22%3A%7B%22content%22%3A%22" + x + "%22%7D%2C%22type%22%3A%22txt%22%7D")
+                    html_doc = link.read().decode()
+                    reply_list = re.findall(r'\"content\":\"(.+?)\\r\\n\"', html_doc)
+                    response = wechat.response_text(reply_list[-1])
             elif message.type == 'image':
                 response = wechat.response_text(u'图片')
             else:
@@ -45,13 +50,7 @@ def sendMsg(request):
     except Exception as e:
         print(e)
 
-    # x = input("主人：")
-    # x = urllib.parse.quote(x)
-    # link = urllib.request.urlopen(
-    #     "http://nlp.xiaoi.com/robot/webrobot?&callback=__webrobot_processMsg&data=%7B%22sessionId%22%3A%22ff725c236e5245a3ac825b2dd88a7501%22%2C%22robotId%22%3A%22webbot%22%2C%22userId%22%3A%227cd29df3450745fbbdcf1a462e6c58e6%22%2C%22body%22%3A%7B%22content%22%3A%22" + x + "%22%7D%2C%22type%22%3A%22txt%22%7D")
-    # html_doc = link.read().decode()
-    # reply_list = re.findall(r'\"content\":\"(.+?)\\r\\n\"', html_doc)
-    # print("小i：" + reply_list[-1])
+
     return HttpResponse(response)
     # try:
     #     xml = request.body
