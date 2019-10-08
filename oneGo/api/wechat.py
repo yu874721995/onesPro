38e31418cd1078bac2fdad7791c1b3b63d634b11
yu874721995 <yubei874721995@163.com>
2019-10-08 17:51:55 +0800

    '1'

--- oneGo/api/wechat.py
+++ oneGo/api/wechat.py
@@ -17,27 +17,29 @@ from wechat_sdk import WechatBasic
 
 
 def sendMsg(request):
-    token = request.GET.get('token',None)
-    openid = request.GET.get('openid',None)
-    signature = request.GET.get('signature',None)
-    timestamp = request.GET.get('timestamp',None)
-    nonce = request.GET.get('nonce',None)
-    wechat = WechatBasic(token=token)
-    body_text = request.body
-    if wechat.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
-        wechat.parse_data(body_text)
-        message = wechat.get_message()
-
+    try:
+        token = request.GET.get('token',None)
+        openid = request.GET.get('openid',None)
+        signature = request.GET.get('signature',None)
+        timestamp = request.GET.get('timestamp',None)
+        nonce = request.GET.get('nonce',None)
+        wechat = WechatBasic(token=token)
+        body_text = request.body
         response = None
-        if message.type == 'text':
-            if message.content == 'wechat':
-                response = wechat.response_text(u'^_^')
+        if wechat.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
+            wechat.parse_data(body_text)
+            message = wechat.get_message()
+            if message.type == 'text':
+                if message.content == 'wechat':
+                    response = wechat.response_text(u'^_^')
+                else:
+                    response = wechat.response_text(u'文字')
+            elif message.type == 'image':
+                response = wechat.response_text(u'图片')
             else:
-                response = wechat.response_text(u'文字')
-        elif message.type == 'image':
-            response = wechat.response_text(u'图片')
-        else:
-            response = wechat.response_text(u'未知')
+                response = wechat.response_text(u'未知')
+    except Exception as e:
+        print(e)
     return HttpResponse(response)
     # try:
     #     xml = request.body
